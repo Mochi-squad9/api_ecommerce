ab39c2613ef1880770407b476b0cb1cd671a7ef1
mirianfalconi <miriahfalconi@gmail.com>
2021-03-28 14:35:29 -0300

    acesso detalhe produto por id

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-# api_ecommerce
+# Flask Api Ecommerce
 
+##
+DADOS DE MATERIAIS ESCOLARES:
 
-Código em Produção:
-https://young-mountain-02170.herokuapp.com/
+```sh
+curl https://young-mountain-02170.herokuapp.com/api
+```
+
+```sh
+curl -X POST https://young-mountain-02170.herokuapp.com/api/getProducts -H "Content-Type: application/json" -d '{"cod": [478167, 174915, 389047]}'
+```
+
+## License
+
+MIT
--- app.py
+++ app.py
@@ -1,45 +1,61 @@
 # -*- coding: utf-8 -*-
-
 import sys
 import requests
 import json
 from bs4 import BeautifulSoup
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def search():
+    return jsonify(PRELOADED)
+
+
+@app.route('/api/getProducts', methods=['POST'])
+def getProducts():
+    content = request.json
+    product_list = []
+    for each in content["cod"]:
+        product_list.append(getProductDetail(each))
+    return jsonify(product_list)
+
+
+def initialSearch():
+    s = connectContext()
+    r = s.get(URL + URL_BUY)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    res = soup.find_all('picture')
+    product = []
+
+    for each in res:
+        img = each.img.get('data-src')
+        if img != None:
+            product_id = img.split('/')[-1].split('.')[0]
+            product.append(getProductDetail(product_id))
+    return product
+
+
+def connectContext():
+    client = requests.session()
+    s = requests.Session()
+    s.headers.update(AGENT)
+    return s
+
+
+def getProductDetail(cod):
+    s = connectContext()
+    r = s.get(URL + URL_PRODUCT + str(cod) + "/")
+    product = r.json()
+    product['produto']['image'] = "https://img.kalunga.com.br/fotosdeprodutos/" + str(cod) + ".jpg"
+    return product['produto']
+
 
 URL = 'https://www.kalunga.com.br/'
 URL_PRODUCT = 'obterProdutoDetalhes/'
 URL_BUY = 'depto/escolar/2?menuID=28'
 AGENT = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0'}
-
-
-@app.route('/')
-def hello_world():
-    return jsonify(arr_img)
-
-
-client = requests.session()
-s = requests.Session()
-s.headers.update(AGENT)
-r = s.get(URL + URL_BUY)
-
-
-soup = BeautifulSoup(r.content, 'html.parser')
-
-res = soup.find_all('picture')
-
-arr_img = []
-
-for each in res:
-    img = each.img.get('data-src')
-    if img != None:
-        product_id = img.split('/')[-1].split('.')[0]
-        r = s.get(URL + URL_PRODUCT + str(product_id) + "/")
-        product = r.json()
-        product['produto']['image'] = img
-        arr_img.append(product['produto'])
+PRELOADED = initialSearch()
 
 if __name__ == "__main__":
     app.run()
